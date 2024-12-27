c8c3332708788f18f8f9d11cec9677a83148fdec
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2024-12-27 09:48:03 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
 from cx_Freeze import setup, Executable
 
-setup(
-    name="Displacement map",
-    version="1.0",
-    description="Displacement map script",
-    executables=[Executable("Displacement map.py")]
-)
+setup(name="Displacement map", executables=[Executable("Displacement map.py")])
