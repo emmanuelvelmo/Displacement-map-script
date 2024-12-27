b759bd147899859e90be944ebc528cceaa65e693
Emmanuel V. <51292782+emmanuelvelmo@users.noreply.github.com>
2024-12-27 11:34:36 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from cx_Freeze import setup, Executable
 
-setup(name="Displacement map", executables=[Executable("Displacement map.py")])
+setup(name="Displacement map", executables=[Executable("Displacement map.py")], options={"build_exe": {"excludes": ["tkinter"]}})
